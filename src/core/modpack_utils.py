e4a9d71a68064fc7ebcd6268b5aac87c77fb8db7
devmoded <matveyykas@protonmail.com>
2026-02-11 14:13:54 +0300

    feat(gui): работа над функционалом установки сборки feat(core): добавление загрузки и распаковки сборки во временный каталог

--- src/core/modpack_utils.py
+++ src/core/modpack_utils.py
@@ -1,9 +1,59 @@
+import requests
+import zipfile
+import io
+import shutil
+import tempfile
+
+from typing import Callable
+from pathlib import Path
+
 class ModpackUtils:
-    def __init__(self, modpack_info: dict[str, str]):
-        self.modpack = modpack_info
+    def __init__(self, modpack_info: dict[str, str], status_callback: Callable):
+        self.name = modpack_info.get('name', '')
+        self.source = modpack_info.get('source', '')
+        self.mc_version = modpack_info.get('mc_version', '')
+        self.modloader = modpack_info.get('modloader', '')
+        self.status = status_callback
 
-    def download_modpack(self):
-        pass
+        self.modpack_temp = None
+        self.modpack_content_path = None
 
-    def print_modpack_info(self):
-        print(self.modpack)
+    def _download_and_extract(self):
+        # Скачивание сборки и сохранение в оперативную память
+        try:
+            response = requests.get(self.source)
+            response.raise_for_status()
+        except requests.HTTPError as e:
+             self.status(f"Ошибка при скачивании сборки: {e}")
+        else:
+            zip_bytes = io.BytesIO(response.content)
+
+            # Создание временного каталога
+            self.modpack_temp = tempfile.TemporaryDirectory()
+            self.modpack_content_path = Path(self.modpack_temp.name)
+
+            # Извлечение содержимого zip во временный каталог
+            try:
+                with zipfile.ZipFile(zip_bytes) as zf:
+                    zf.extractall(self.modpack_content_path)
+            except zipfile.BadZipFile:
+                self.status("Ошибка: архив повреждён")
+            except OSError as e:
+                self.status(f"Ошибка записи на диск: {e}")
+            else:
+                self.status(f"Cборка '{self.name}' успешно скачана и распакована в {self.modpack_content_path}")
+
+    def _cleanup(self):
+        # Очистка временного каталога
+        if self.modpack_temp:
+            self.modpack_temp.cleanup()
+            self.modpack_temp = None
+            self.modpack_content_path = None
+            self.status('Временный каталог удалён')
+
+    def self_install(self):
+        self.status(f"Начата установка сборки '{self.name}'")
+        self._download_and_extract()
+
+        self._cleanup()
+        self.status('Готово')
--- src/gui.py
+++ src/gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import threading
 
 from tkinter import ttk
+from queue import Queue
 from requests import HTTPError
 
 from config import INDEX_URL
@@ -28,6 +29,9 @@ class MainFrame(ttk.Frame):
 
         self.columnconfigure(0, weight=1)
         self.create_widgets()
+        self.queue = Queue()
+        self._checking_queue = False
+        self._installing = False
         self.index = None
 
         threading.Thread(target=self._load_index, daemon=True).start()
@@ -58,9 +62,48 @@ class MainFrame(ttk.Frame):
 
     def _on_modpack_changed(self, *args):
         if self.sel_modpack_name.get():
-            self.install_button.state(["!disabled"])
+            self.install_button.config(state='normal')
         else:
-            self.install_button.state(["disabled"])
+            self.install_button.config(state='disabled')
+
+    def _start_install(self):
+        if self._installing:
+            return
+
+        self._installing = True
+        self.install_button.config(state='disabled')
+
+        self._checking_queue = True
+        threading.Thread(target=self._installer, daemon=True).start()
+        self._check_queue()
+
+    def _installer(self):
+        selected = self.sel_modpack_name.get() # Получение названия выбранной сборки
+        if not selected:
+            self._set_status('Выберите сборку!')
+            return
+
+        self._set_status(f"Получение информации о сборке '{selected}'")
+        modpack_info = index_utils.modpack_query(self.index, selected)
+        if modpack_info:
+            modpack_utils = ModpackUtils(modpack_info, status_callback=self.queue.put)
+            modpack_utils.self_install()
+        else:
+            self._set_status(f"Информация о сборке '{selected}' пуста")
+
+    def _check_queue(self):
+        if not self._checking_queue:
+            return
+
+        while not self.queue.empty():
+            message = self.queue.get()
+            self._set_status(message)
+            if message == 'Готово':
+                self._checking_queue = False
+                self._installing = False
+                self.install_button.config(state='normal')
+
+        self.after(100, self._check_queue)
 
     def create_widgets(self):
         # Поле с состоянием работы программы
@@ -90,25 +133,9 @@ class MainFrame(ttk.Frame):
         # Кнопка установки
         self.install_button = ttk.Button(
             self, text='Установить', state='disabled',
-            command=self.on_install
+            command=self._start_install
         )
         self.install_button.grid(row=2, column=0, pady=15)
-
-    def on_install(self):
-        selected = self.sel_modpack_name.get() # Получение названия выбранной сборки
-        if not selected:
-            self._set_status('Выберите сборку!')
-            return
-
-        self._set_status(f"Получение информации о сборке '{selected}'")
-        modpack_info = index_utils.modpack_query(self.index, selected)
-        if modpack_info:
-            modpack_utils = ModpackUtils(modpack_info)
-            self._set_status(f"Скачивание сборки '{selected}'")
-            # TODO: Сделать установку сборок
-            modpack_utils.print_modpack_info()
-        else:
-            self._set_status(f"Информация о сборке '{selected}' пуста")
 
 app = App()
 app.mainloop()
